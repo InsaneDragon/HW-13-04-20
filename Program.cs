8d65e72d2d110280c920b9701279fc119826b0a0
C#Developer <shahzodkiller11@gmail.com>
2020-04-05 21:02:08 +0500

    Conmpleted(With Comments)

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace Classes2
     {
         static void Main(string[] args)
         {
-            Converter currentCurrency = new Converter(10.22, 11.4, 0.13);
+            Converter currentCurrency = new Converter(10.22, 11.4, 0.13);//отправляю классу курсы валют
             Console.Write("Write How much money you want to Convert:");
-            double money = double.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());//получаю количество денег 
             Console.WriteLine("1.Somoni to USD");
             Console.WriteLine("2.Somoni to Ruble");
             Console.WriteLine("3.Somoni to Euro");
@@ -18,17 +18,17 @@ namespace Classes2
             Console.WriteLine("5.Ruble to Somoni");
             Console.WriteLine("6.Euro to Somoni");
             Console.Write("Choose number from list above:");
-            int Currency = int.Parse(Console.ReadLine());
-            double after=Classes2.Program.Currency(money,Currency,currentCurrency);
+            int Currency = int.Parse(Console.ReadLine());//из какой валюты в какую делать конвертацию
+            double after=Classes2.Program.Currency(money,Currency,currentCurrency);//получаю зарплату
             Console.WriteLine($"Your Money:{after}");
             System.Console.WriteLine("////////////////////////////////////////////////////3");
-            System.Console.Write("Write Employee Name:");
+            System.Console.Write("Write Employee Name:");//получаю имя
             string Name=Console.ReadLine();
-            System.Console.Write("Write Employee Surname:");
+            System.Console.Write("Write Employee Surname:");//получаю фамилию
             string SurName=Console.ReadLine();
             List<string>listofprofessions=new List<string>(){"teacher","programmer","OfficeEmployee","Manager"};
             int count=1;
-            foreach(var item in listofprofessions)
+            foreach(var item in listofprofessions)//показываю все доступные профессии из листа 
             {
                 System.Console.WriteLine($"{count}. {item}");
                 count++;
@@ -36,15 +36,15 @@ namespace Classes2
             System.Console.Write("Choose Profession from list above:");
             int Profession=int.Parse(Console.ReadLine());
             System.Console.WriteLine("Write Employees Experience(How many year he is working):");
-            int Experience=int.Parse(Console.ReadLine());
+            int Experience=int.Parse(Console.ReadLine());//получаю опыт работы
             double Salary=Classes2.Program.Salary(Profession);
-            Salary+=Experience*1000;
+            Salary+=Experience*1000;//прибавляю к зарплате стаж за каждй год работы плюс 1000
             string activity=listofprofessions[Profession-1];
             Employee Human=new Employee(){Name=Name,SurName=SurName,Profession=activity,Expirience=Experience};
-            double Taxes=Salary*0.13;
-            System.Console.WriteLine($"Name:{Human.Name} SurName:{Human.SurName} Profession:{Human.Profession} Experience:{Human.Expirience} Salary:{Salary} Tax Levy:{Taxes}");
+            double Taxes=Salary*0.13;//Налог 13%
+            System.Console.WriteLine($"Name:{Human.Name} SurName:{Human.SurName} Profession:{Human.Profession} Experience:{Human.Expirience} Salary:{Salary} Tax Levy:{Taxes}");//показываю результат
         }
-        public static double Currency(double money, int Currency,Converter Current)
+        public static double Currency(double money, int Currency,Converter Current)//метод для обмена валют
         {
             double after=0;
             switch (Currency)
@@ -58,7 +58,7 @@ namespace Classes2
             }
             return after;
         }
-        public static double Salary(int Profession)
+        public static double Salary(int Profession)//проверяю зарплату по професии
         {
             double Money=0;
             switch(Profession)
@@ -71,7 +71,7 @@ namespace Classes2
             return Money;
         }
     }
-    public class Converter
+    public class Converter //класс с курсами валют
     {
         public double som { get; set; }
         public double dollar { get; set; }
@@ -79,7 +79,7 @@ namespace Classes2
         public double ruble { get; set; }
         public Converter(double usd, double eur, double rub) { dollar = usd; euro = eur; ruble = rub; }
     }
-    public class Employee
+    public class Employee ///Создал класс Работник
     {
         public string Name { get; set; }
         public string SurName { get; set; }
