f1ce9acd03d61a93caaa608117d4d484c77c9f9d
C#Developer <shahzodkiller11@gmail.com>
2020-04-08 19:18:43 +0500

    Pop

--- Program.cs
+++ Program.cs
@@ -8,84 +8,45 @@ namespace Classes2
     {
         static void Main(string[] args)
         {
-            Converter currentCurrency = new Converter(10.22, 11.4, 0.13);//отправляю классу курсы валют
-            Console.Write("Write How much money you want to Convert:");
-            double money = double.Parse(Console.ReadLine());//получаю количество денег 
-            Console.WriteLine("1.Somoni to USD");
-            Console.WriteLine("2.Somoni to Ruble");
-            Console.WriteLine("3.Somoni to Euro");
-            Console.WriteLine("4.USD to Somoni");
-            Console.WriteLine("5.Ruble to Somoni");
-            Console.WriteLine("6.Euro to Somoni");
-            Console.Write("Choose number from list above:");
-            int Currency = int.Parse(Console.ReadLine());//из какой валюты в какую делать конвертацию
-            double after=Classes2.Converter.Currency(money,Currency,currentCurrency);
-            Console.WriteLine($"Your Money:{after}");// показываю деньги после конвертации
-            System.Console.WriteLine("////////////////////////////////////////////////////3");
-            System.Console.Write("Write Employee Name:");//получаю имя
-            string Name=Console.ReadLine();
-            System.Console.Write("Write Employee Surname:");//получаю фамилию
-            string SurName=Console.ReadLine();
-            List<string>listofprofessions=new List<string>(){"teacher","programmer","OfficeEmployee","Manager"};
-            int count=1;
-            foreach(var item in listofprofessions)//показываю все доступные профессии из листа 
-            {
-                System.Console.WriteLine($"{count}. {item}");
-                count++;
-            }
-            System.Console.Write("Choose Profession from list above:");
-            int Profession=int.Parse(Console.ReadLine());
-            System.Console.WriteLine("Write Employees Experience(How many year he is working):");
-            int Experience=int.Parse(Console.ReadLine());//получаю опыт работы
-            double Salary=Classes2.Program.Salary(Profession);//получаю зарплату
-            Salary+=Experience*1000;//прибавляю к зарплате стаж за каждй год работы плюс 1000
-            string activity=listofprofessions[Profession-1];
-            Employee Human=new Employee(){Name=Name,SurName=SurName,Profession=activity,Expirience=Experience};
-            double Taxes=Salary*0.13;//Налог 13%
-            System.Console.WriteLine($"Name:{Human.Name} SurName:{Human.SurName} Profession:{Human.Profession} Experience:{Human.Expirience} Salary:{Salary} Tax Levy:{Taxes}");//показываю результат
+            int[] b = new int[8] { 1, 2, 3, 4, 5, 6, 7, 7 };
+            int n = ArrayHelper.Pop(ref b);
+            System.Console.WriteLine(n);
+            
         }
-        
-        public static double Salary(int Profession)//проверяю зарплату по професии
+        public class ArrayHelper
         {
-            double Money=0;
-            switch(Profession)
+            public static int Pop(ref int[] list)
             {
-                case 1:Money=1200;break;
-                case 2:Money=24000;break;
-                case 3:Money=2400;break;
-                case 4:Money=3600;break;
+                int a = list[list.Length - 1];
+                int[] b = new int[list.Length - 1];
+                for (int i = 0; i < list.Length - 1; i++) b[i] = list[i];
+                list = b;
+                return a;
             }
-            return Money;
-        }
-    }
-    public class Converter //класс с курсами валют
-    {
-        public double som { get; set; }
-        public double dollar { get; set; }
-        public double euro { get; set; }
-        public double ruble { get; set; }
-        public Converter(double usd, double eur, double rub) { dollar = usd; euro = eur; ruble = rub; }
-
-        public static double Currency(double money, int Currency,Converter Current)//метод для обмена валют
-        {
-            double after=0;
-            switch (Currency)
+            public static double Pop(ref double[] list)
             {
-                case 1: after=money*Current.dollar;break;
-                case 2: after= money*Current.ruble;break;
-                case 3: after= money*Current.euro;break;
-                case 4: after= money*0.098;break;
-                case 5: after= money*7.48;break;
-                case 6: after= money*0.91;break;
+                double a = list[list.Length - 1];
+                double[] b = new double[list.Length - 1];
+                for (int i = 0; i < list.Length - 1; i++) b[i] = list[i];
+                list = b;
+                return a;
             }
-            return after;
+            public static decimal Pop(ref decimal[] list)
+            {
+                decimal a = list[list.Length - 1];
+                decimal[] b = new decimal[list.Length - 1];
+                for (int i = 0; i < list.Length - 1; i++) b[i] = list[i];
+                list = b;
+                return a;
+            }
+            public static float Pop(ref float[] list)
+            {
+                float a = list[list.Length - 1];
+                float[] b = new float[list.Length - 1];
+                for (int i = 0; i < list.Length - 1; i++) b[i] = list[i];
+                list = b;
+                return a;
+            }
         }
-    }
-    public class Employee ///Создал класс Работник
-    {
-        public string Name { get; set; }
-        public string SurName { get; set; }
-        public string Profession{get;set;}
-        public int Expirience{get;set;}
     }
 }
