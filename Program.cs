a58b7e7e63e9849b954c189501d650435267b830
C#Developer <shahzodkiller11@gmail.com>
2020-04-08 19:19:31 +0500

    Push

--- Program.cs
+++ Program.cs
@@ -47,6 +47,38 @@ namespace Classes2
                 list = b;
                 return a;
             }
+            public static int Push(ref int[] list, int num)
+            {
+                int[] b = new int[list.Length + 1];
+                for (int i = 0; i < list.Length; i++) b[i] = list[i];
+                b[b.Length - 1] = num;
+                list = b;
+                return b.Length;
+            }
+            public static int Push(ref double[] list, int num)
+            {
+                double[] b = new double[list.Length + 1];
+                for (int i = 0; i < list.Length; i++) b[i] = list[i];
+                b[b.Length - 1] = num;
+                list = b;
+                return b.Length;
+            }
+            public static decimal Push(ref decimal[] list, int num)
+            {
+                decimal[] b = new decimal[list.Length + 1];
+                for (int i = 0; i < list.Length; i++) b[i] = list[i];
+                b[b.Length - 1] = num;
+                list = b;
+                return b.Length;
+            }
+            public static float Push(ref float[] list, int num)
+            {
+                float[] b = new float[list.Length + 1];
+                for (int i = 0; i < list.Length; i++) b[i] = list[i];
+                b[b.Length - 1] = num;
+                list = b;
+                return b.Length;
+            }
         }
     }
 }
